68a8422cb0b86bb3d6fc7699c6fbae715555c108
stevo9061 <sbittgen1@gmail.com>
2023-03-11 22:12:24 +0100

    Added Ethereum & Liquid folders

---
+++ Ethereum_API/ethereum_api.py
@@ -0,0 +1,49 @@
+import json
+from datetime import datetime
+import requests
+
+
+def get_avg_transactions_block():
+    url= "https://api.blockchair.com/ethereum/stats"
+    response = requests.get(url).text
+    response_to_json = json.loads(response)
+    transactions_24h = response_to_json['data']['transactions_24h']
+    blocks_24h = response_to_json['data']['blocks_24h']
+
+    avg_transactions_block = transactions_24h / blocks_24h
+# Fulfill first requirement
+    print(f"The average transactions per block are {avg_transactions_block}")
+    ethereum_tps_file.write(f"\nThe average transactions per block are {avg_transactions_block}")
+
+    return avg_transactions_block
+
+
+# Open file stream
+# Append new report to the end of the file
+ethereum_tps_file = open("C:\\Users\\sbitt\PycharmProjects\\pythonProject\\Ethereum_API\\ethereum_tps.txt", "a")
+current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+ethereum_tps_file.write(f"\nToday's date is {current_time}")
+
+avg_tx_block = get_avg_transactions_block()
+
+# No measurable fluctuation rate of the block time anymore
+# (since switching from PoW to PoS -> 12.06 lowest - 12.25 highest)
+# -> since no mining difficulty anymore
+# https://ycharts.com/indicators/ethereum_average_block_time
+# https://ethereum.org/en/developers/docs/blocks/
+ethereum_blocktime = 12  # seconds
+
+ethereum_tps = avg_tx_block / ethereum_blocktime
+
+# Fulfill second requirement
+print("The blocktime on the Ethereum blockchain is about 12 seconds")
+ethereum_tps_file.write("\nThe blocktime on the Ethereum blockchain is about 12 seconds")
+
+
+print(f"\nEthereum: Number of transactions per second: {ethereum_tps:.2f}")
+ethereum_tps_file.write(f"\nNumber of transactions per second: {ethereum_tps:.2f}\n")
+
+
+# 1 get average transactions per block
+# 2 get average block time in seconds
+# https://medium.com/@alephium/transactions-per-second-tps-f13217a49e39
---
+++ Liquid_API/liquid_api_txt.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import requests
+
+
+def get_latest_block():
+    url = "https://blockstream.info/liquid/api/blocks/tip/height"
+    response = requests.get(url).text # e.g. 2218887
+    return response
+
+
+def get_block_hash(block_height):
+    url = f"https://blockstream.info/liquid/api/block-height/{block_height}"
+    response = requests.get(url).text
+    return response
+
+
+def get_tx_count(block_hash):
+    url = f"https://blockstream.info/liquid/api/block/{block_hash}"
+    response = requests.get(url)
+    return response.json()["tx_count"] # e.g. 1539
+
+# Open file stream
+# Append new report to the end of the file
+liquid_tps_file = open("C:\\Users\\sbitt\PycharmProjects\\pythonProject\\Liquid_API\\liquid_tps.txt", "a")
+
+current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+liquid_tps_file.write(f"\nToday's date is {current_time}")
+
+
+block_count = int(get_latest_block())
+tx_count = 0
+# (60 x 1 = 1 minutes) https://help.blockstream.com/hc/en-us/articles/900001390903-What-is-the-transaction-capacity-of-Liquid-
+# wrong -> (60 x 2 = 2 minutes)https://hackernoon.com/a-beginners-guide-to-the-liquid-network
+block_time_sec = 60
+blocksToCalc = 100
+
+for i in range(block_count, block_count - blocksToCalc, -1):
+    block_hash = get_block_hash(i)
+    liquid_tps_file.write(f"\nBlockcount: {i} Blockhash: {block_hash}")
+    tx_count += get_tx_count(block_hash)
+    liquid_tps_file.write(f"\nTransaction count = {tx_count}")
+
+current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+print(f"\nToday's date is {current_time}")
+print(f"\nLiquid Network: In the most recent and last {blocksToCalc} blocks {tx_count} transactions were performed *.* ")
+liquid_tps_file.write(f"\nLiquid Network: In the most recent and last {blocksToCalc} blocks {tx_count} transactions were performed *.* ")
+
+avgTransactionsPerBlock = tx_count / blocksToCalc
+
+# Fulfill first requirement
+print(f"The average transactions per block are {avgTransactionsPerBlock}")
+liquid_tps_file.write(f"\nThe average transactions per block are {avgTransactionsPerBlock}")
+
+# Fulfill second requirement
+print(f"The blocktime on the Liquid Network L2 Off-Chain solution is about {block_time_sec} minutes.")
+liquid_tps_file.write(f"\nThe blocktime on the Liquid Network L2 Off-Chain solution is about {block_time_sec} minutes.")
+
+liquid_tps = avgTransactionsPerBlock / block_time_sec
+
+
+print(f"Number of transactions per second: {liquid_tps:.2f}")
+liquid_tps_file.write(f"\nNumber of transactions per second: {liquid_tps:.2f}\n\n")
+
+# 1 get average transactions per block
+# 2 get average block time in seconds
+# https://medium.com/@alephium/transactions-per-second-tps-f13217a49e39
+# https://blockstream.info/liquid/blocks/recent
