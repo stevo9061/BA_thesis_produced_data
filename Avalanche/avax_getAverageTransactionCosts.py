9b28b74cba8e7252a222972d14393aa6e589bce3
stevo9061 <sbittgen1@gmail.com>
2023-04-12 21:58:13 +0200

    customized script

--- Avalanche/avax_getAverageTransactionCosts.py
+++ Avalanche/avax_getAverageTransactionCosts.py
@@ -11,7 +11,7 @@ averageTransactionCosts = 0
 transactionCounter = 0
 leastExpensiveTransaction = 1
 mostExpensiveTransaction = 0
-
+avalanche_costs_file = open("C:\\Users\\sbitt\PycharmProjects\\pythonProject\\Avalanche\\avalanche_costs.txt", "w")
 
 for keyVal in data['ethereum']['transactions']:
     averageTransactionCosts += keyVal['average']
@@ -30,10 +30,14 @@ for iterator in data['ethereum']['transactions']:
         mostExpensiveTransaction = iterator['average']
 
 print()
-print("On Average, the transaction costs are", averageTransactionCosts, "AVAX, and $", 17.67*averageTransactionCosts, "USD")
+print("On Average, the transaction cost is", averageTransactionCosts,  "AVAX, and $", 17.67*averageTransactionCosts, "USD")
+avalanche_costs_file.write(f"On Average, the transaction cost is {averageTransactionCosts} AVAX, and $ {17.67*averageTransactionCosts} USD\n")
 print("On 08/04/2023 the AVAX price is $ 17.67 USD")
-print("The most expensive transaction costs", mostExpensiveTransaction, "AVAX, and $", 17.67*mostExpensiveTransaction, "USD")
-print("The least expensive transaction costs", leastExpensiveTransaction, "AVAX, and $", 17.67*leastExpensiveTransaction, "USD")
+avalanche_costs_file.write(f"On 08/04/2023 the AVAX price is $ 17.67 USD\n")
+print("The most expensive transaction has cost", mostExpensiveTransaction, "AVAX, and $", 17.67*mostExpensiveTransaction, "USD")
+avalanche_costs_file.write(f"The most expensive transaction has cost {mostExpensiveTransaction} AVAX, and $ {17.67*mostExpensiveTransaction} USD\n")
+print("The least expensive transaction has cost", leastExpensiveTransaction, "AVAX, and $", 17.67*leastExpensiveTransaction, "USD")
+avalanche_costs_file.write(f"The least expensive transaction has cost {leastExpensiveTransaction} AVAX, and $ {17.67*leastExpensiveTransaction} USD\n")
 
 
 
